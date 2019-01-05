3ffa8023907bb5b9aef93d64bc719e807217af78
Bill LeBorgne <bill@leborgne.name>
2019-01-05 15:10:00 -0700

    Parse paths from requests to server
    From "Parsing Request Paths" video, parse the path of the
    requested URL, and log it (removing leading/trailing '/' chars)

--- index.js
+++ index.js
@@ -4,11 +4,24 @@
 
 // Dependencies
 const http = require('http');
+const url = require('url');
 
 // Server responds to all requests with a string
 var server = http.createServer(function(req, resp){
-	resp.end('Hello, world!\n');
-})
+  // parse url
+  let parsedUrl = url.parse(req.url, true);
+ 
+  // get path
+  let path = parsedUrl.pathname;
+  let trimmedPath = path.replace(/^\/+|\/+$/g,'');
+
+  // send response
+  resp.end('Hello, World!\n');
+
+  // log request path
+  console.log('Request received for path ' + trimmedPath);
+});
+
 // Start the server, listen on port 3000
 server.listen(3000, function(){
   console.log("Server listening on port 3000.");
