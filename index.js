25672aa63bcc91e3aa8a20ecdf3e5ecee981b891
Bill LeBorgne <bill@leborgne.name>
2019-01-06 15:15:05 -0700

    Parse other things out of request
    Add parsing and logging of HTTP method, query string, headers
    and request body

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 // Dependencies
 const http = require('http');
 const url = require('url');
+const { StringDecoder } = require('string_decoder');
 
 // Server responds to all requests with a string
 var server = http.createServer(function(req, resp){
@@ -15,11 +16,34 @@ var server = http.createServer(function(req, resp){
   let path = parsedUrl.pathname;
   let trimmedPath = path.replace(/^\/+|\/+$/g,'');
 
-  // send response
-  resp.end('Hello, World!\n');
+  // get query string as an object
+  let queryStringObject = parsedUrl.query;
+ 
+  // get http method
+  let method = req.method.toLowerCase();
 
-  // log request path
-  console.log('Request received for path ' + trimmedPath);
+  // get headers as object
+  let headers = req.headers;
+
+  // get request payload if present
+  let decoder = new StringDecoder('utf-8');
+  let buffer = '';
+  req.on('data', function(data){
+    buffer += decoder.write(data)
+  });
+  req.on('end', function(){
+    buffer += decoder.end();
+
+    // send response
+    resp.end('Hello, World!\n');
+
+    // log request path
+    console.log(`Received ${method} request for path ${trimmedPath} with query params:`);
+    console.log(queryStringObject);
+    console.log('and headers:');
+    console.log(headers);
+    console.log(`with payload: \"${buffer}\"`);
+  });
 });
 
 // Start the server, listen on port 3000
